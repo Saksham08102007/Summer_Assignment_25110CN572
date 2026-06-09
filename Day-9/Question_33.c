152d7954c16fc026ba42f38b4f878f043fd5cd78
Saksham <sakshamgupta736@gmail.com>
2026-06-09 17:09:41 +0530

    Day-9 uploaded

---
+++ Day-9/Question_33.c
@@ -0,0 +1,23 @@
+/* 
+Write a program to Print reverse star pattern.
+*****
+****
+***
+**
+*
+ */
+
+#include <stdio.h> 
+
+int main () {
+    int i,j,n;
+    printf("Enter number of rows: "); // n = 5
+    scanf("%d",&n);
+    for (i=1;i<=n;i++) {
+        for (j=n;j>=i;j--) {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Day-9/Question_34.c
@@ -0,0 +1,24 @@
+/* 
+Write a program to Print reverse number
+triangle.
+12345
+1234
+123
+12
+1
+*/
+
+#include <stdio.h>
+
+int main () {
+    int i,j,n;
+    printf("Enter number of rows: "); // n = 5
+    scanf("%d",&n);
+    for (i=1;i<=n;i++) {
+        for (j=1;j<=n-i+1;j++) {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
