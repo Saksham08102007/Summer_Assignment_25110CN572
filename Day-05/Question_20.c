a2243e31e0ace3354c92cb5233f0e7e64944b4f8
Saksham <sakshamgupta736@gmail.com>
2026-06-11 14:08:56 +0530

    Assignments uploaded

--- Day-05/Question_20.c
+++ Day-05/Question_20.c
@@ -7,11 +7,14 @@ int main () {
     printf("Enter a number: ");
     scanf("%d",&n);
     temp = n;
-    for(i=2;i<=n;i++) {
+    for(i=2;i*i<=n;i++) {
         while (n % i == 0) {
             largest = i;
             n = n / i;
         }
+    }
+    if (n>1) {
+        largest = n;
     }
     printf("Largest prime factor of %d is: %d",temp,largest);
     return 0;
